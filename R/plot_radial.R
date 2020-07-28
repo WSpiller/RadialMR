c74072b0cb81ce0482f598ecc15a5d9dbeb35263
Tom Palmer <remlapmot@hotmail.com>
2020-07-28 21:13:27 +0100

    import functions

--- R/plot_radial.R
+++ R/plot_radial.R
@@ -11,6 +11,7 @@
 #'@references Bowden, J., et al., Improving the visualization, interpretation and analysis of two-sample summary data Mendelian randomization via the Radial plot and Radial regression. International Journal of Epidemiology, 2018. 47(4): p. 1264-1278.
 #'@export
 #'@examples
+#' @importFrom stats median quantile
 #'
 #' \dontrun{
 #' plot_radial(r_object,TRUE,TRUE,TRUE)
