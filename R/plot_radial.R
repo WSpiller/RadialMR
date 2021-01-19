55bcb3e9a13dcf64ef8187476c5661e206fdd34b
Tom Palmer <remlapmot@hotmail.com>
2021-01-19 11:05:29 +0000

    reformat example

--- R/plot_radial.R
+++ R/plot_radial.R
@@ -10,3 +10,10 @@
 #' @author Wes Spiller; Jack Bowden; Tom Palmer.
 #' @references Bowden, J., et al., Improving the visualization, interpretation and analysis of two-sample summary data Mendelian randomization via the Radial plot and Radial regression. International Journal of Epidemiology, 2018. 47(4): p. 1264-1278.
 #' @importFrom stats quantile median
+#' @export
+#' @examples
+#' ldl.dat <- data_radial[data_radial[,10]<5*10^-8,]
+#' ldl.fdat <- format_radial(ldl.dat[,6], ldl.dat[,9],
+#'                           ldl.dat[,15], ldl.dat[,21], ldl.dat[,1])
+#' ivw.object <- ivw_radial(ldl.fdat, 0.05, 1, 0.0001, TRUE)
+#' plot_radial(ivw.object)
