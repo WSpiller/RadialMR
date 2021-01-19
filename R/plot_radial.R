df4e0eea6543f1d9d7f51bd8f34e756e1c8f14f5
Tom Palmer <remlapmot@hotmail.com>
2021-01-19 11:05:15 +0000

    import median

--- R/plot_radial.R
+++ R/plot_radial.R
@@ -9,3 +9,4 @@
 #' @return A ggplot object containing a radial plot of either the IVW, MR-Egger, or both estimates simultaneously.
 #' @author Wes Spiller; Jack Bowden; Tom Palmer.
 #' @references Bowden, J., et al., Improving the visualization, interpretation and analysis of two-sample summary data Mendelian randomization via the Radial plot and Radial regression. International Journal of Epidemiology, 2018. 47(4): p. 1264-1278.
+#' @importFrom stats quantile median
