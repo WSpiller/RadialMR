3cbac1420d8f72d5b811b8526e35210b257d125f
Tom Palmer <remlapmot@hotmail.com>
2023-05-05 14:58:45 +0100

    Delete importFrom lines

--- R/egger_radial.R
+++ R/egger_radial.R
@@ -17,7 +17,6 @@
 #'}
 #' @author Wes Spiller; Jack Bowden; Tom Palmer.
 #' @references Bowden, J., et al., Improving the visualization, interpretation and analysis of two-sample summary data Mendelian randomization via the Radial plot and Radial regression. International Journal of Epidemiology, 2018. 47(4): p. 1264-1278.
-#' @importFrom stats lm coef confint optimize pchisq pf pnorm pt qchisq qt sd
 #' @export
 #' @examples
 #' # Example using format_radial data
