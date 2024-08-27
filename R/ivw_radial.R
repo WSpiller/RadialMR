11bc4763b5f03571e77a8817dfdd7ea47578b888
Tom Palmer <remlapmot@hotmail.com>
2024-08-27 14:22:28 +0100

    lintr

--- R/ivw_radial.R
+++ R/ivw_radial.R
@@ -70,7 +70,7 @@ ivw_radial<-function(r_input,alpha,weights,tol,summary){
                     "se.outcome",
                     "SNP",
                     "mr_keep")
-    for (i in 1:length(cnamesneed)) {
+    for (i in seq_along(cnamesneed)) {
       if (!(cnamesneed[i] %in% colnames(r_input))) {
         stop(paste('This data.frame does not have the required column',
                    cnamesneed[i]))
@@ -403,7 +403,7 @@ ivw_radial<-function(r_input,alpha,weights,tol,summary){
   Qj_Chi<-0
 
   # Perform chi square tests for each Q contribution Qj
-  for(i in 1:length(Qj)){
+  for(i in seq_along(Qj)){
     Qj_Chi[i]<-stats::pchisq(Qj[i],1,lower.tail = FALSE)
   }
 
@@ -415,8 +415,8 @@ ivw_radial<-function(r_input,alpha,weights,tol,summary){
   Out_Indicator<-rep(0,length(r_input[,2]))
 
   # Include value of 1 indicating positive outlier status for given sig.threshold
-  for( i in 1:length(r_input[,2])){
-    if(Qj_Chi[i]<alpha){
+  for (i in seq_along(r_input[,2])) {
+    if (Qj_Chi[i]<alpha) {
       Out_Indicator[i]<-1
     }
   }
